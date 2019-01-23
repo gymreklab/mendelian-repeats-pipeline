08ab1aa15e6c86f69558f06c6d750ad1dbce5844
Gymrek Lab <melissa.gymrek@gmail.com>
2019-01-22 20:30:01 -0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Usage: ./run.sh <configfile>
+
+CONFIG=$1
+
+die()
+{
+    BASE=$(basename "$0")
+    echo "$BASE error: $1" >&2
+    exit 1
+}
+
+./1_gangstr.sh $CONFIG || die "Error running GangSTR"
+./2_dumpstr.sh $CONFIG || die "Error running DumpSTR"
+
+exit 0
