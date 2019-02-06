0c1ff21ca5b0f04b2e62b7c17fc6f8867ea91528
richyanicky <richyanicky@gmail.com>
2019-02-06 13:50:00 -0800

    Update 1_gangstr.sh
    Added param checks

--- 1_gangstr.sh
+++ 1_gangstr.sh
@@ -3,7 +3,7 @@
 # Usage: ./1_gangstr.sh <configfile>
 
 CONFIG="$1"
-
+params=""
 source $CONFIG
 
 die()
@@ -13,13 +13,53 @@ die()
     exit 1
 }
 
-/home/ryanicky/bin/GangSTR --bam $bams \
-        --bam-samps $bamsamps \
-        --ref $ref \
-        --regions $regions \
-        --out $out \
-        --str-info $strinfo \
-        --chrom $chrom \
-        --period $period
-|| die "Error running GangSTR"
+if [[ $bams ]]; then
+params="--bam "$bams
+else 
+die "no bam file specified"
+fi
+
+if [[ $bamsamps ]]; then
+params=$params" --bam-samps "$bamsamps
+fi
+
+if [[ $ref ]]; then
+params=$params" --ref "$ref
+else
+die "no ref file specified"
+fi
+ 
+if [[ $regions ]]; then
+params=$params" --regions "$regions
+else
+die "no regions file specified"
+fi
+
+if [[ $out ]]; then
+params=$params" --out "$out
+fi
+
+if [[ $strinfo ]]; then
+params=$params" --str-info "$strinfo
+fi
+
+if [[ $chrom ]]; then
+params=$params" --chrom "$chrom
+fi
+
+if [[ $period ]]; then
+params=$params" --period "$period
+fi
+
+echo $params
+
+#/home/ryanicky/bin/GangSTR --bam $bams \
+#        --bam-samps $bamsamps \
+#        --ref $ref \
+#        --regions $regions \
+#        --out $out \
+#        --str-info $strinfo \
+#        --chrom $chrom \
+#        --period $period \
+/home/ryanicky/bin/GangSTR $params || die "Error running GangSTR"
 exit 0 # Not implemented
