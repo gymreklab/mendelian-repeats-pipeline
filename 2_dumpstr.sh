ed90a781ae598e55b5d214431a92d2e6cad2ceb5
richyanicky <richyanicky@gmail.com>
2019-02-06 13:51:48 -0800

    Update 2_dumpstr.sh
    Added Param checks

--- 2_dumpstr.sh
+++ 2_dumpstr.sh
@@ -3,9 +3,9 @@
 # Usage: ./2_dumpstr.sh <configfile>
 
 CONFIG="$1"
-
+params=""
 soruce $CONFIG
-i
+
 die()
 {
     BASE=$(basename "$0")
@@ -13,11 +13,37 @@ die()
     exit 1
 }
 
-./home/ryanicky/workspace/STRTools/scripts/dumpSTR/dumpSTR.py --vcf $vcf \
-             --out $outdstr \
-             --filter-regions $filterregions \
-             --filter-regions-names $filterregionsnames \
-             --min-total-reads $mintotalreads
+if [[ $vcf ]]; then
+params="--vcf "$vcf
+else 
+die "no vcf file specified"
+fi
+
+if [[ $filterregions ]]; then
+params=$params" --filter-regions "$filterregions
+else
+die "no regions file specified"
+fi
+ 
+if [[ $filterregionsnames ]]; then
+params=$params" --filter-regions-name "$filterregionsnames
+else
+die "no regions names file specified"
+fi
+
+if [[ $outdstr ]]; then
+params=$params" --out "$outdstr
+fi
+
+echo $params
+
+./home/ryanicky/workspace/STRTools/scripts/dumpSTR/dumpSTR.py $params || die exit 1
+
+#./home/ryanicky/workspace/STRTools/scripts/dumpSTR/dumpSTR.py --vcf $vcf \
+#             --out $outdstr \
+#             --filter-regions $filterregions \
+#             --filter-regions-names $filterregionsnames \
+#             --min-total-reads $mintotalreads
 
 
 exit 0 # Not implemented
