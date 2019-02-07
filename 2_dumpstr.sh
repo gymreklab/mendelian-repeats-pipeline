5a8bcebbfb1f15227184d462ccd5150bc095a128
richyanicky <richyanicky@gmail.com>
2019-02-07 13:06:37 -0800

    Update 2_dumpstr.sh
    Fixed typos

--- 2_dumpstr.sh
+++ 2_dumpstr.sh
@@ -2,9 +2,11 @@
 
 # Usage: ./2_dumpstr.sh <configfile>
 
+echo $(date '+%Y %b %d %H:%M') dumpSTR started 
+
 CONFIG="$1"
 params=""
-soruce $CONFIG
+source $CONFIG
 
 die()
 {
@@ -35,15 +37,32 @@ if [[ $outdstr ]]; then
 params=$params" --out "$outdstr
 fi
 
+if [[ $mintotalreads ]]; then
+params=$params" --min-total-reads "$mintotalreads
+fi
+
+if [[ $expansionprobhet ]]; then
+params=$params" --expansion-prob-het "$expansionprobhet
+fi
+
+if [[ $expansionprobhom ]]; then
+params=$params" --expansion-prob-hom "$expansionprobhom
+fi
+
+if [[ $expansionprobtotal ]]; then
+params=$params" --expansion-prob-total "$expansionprobtotal
+fi
+
+if [[ $filterspanonly ]]; then
+params=$params" --filter-span-only "$filterspanonly 
+fi
+
+echo Paramters used:
 echo $params
 
-./home/ryanicky/workspace/STRTools/scripts/dumpSTR/dumpSTR.py $params || die exit 1
+/home/ryanicky/workspace/STRTools/scripts/dumpSTR/dumpSTR.py $params || die exit 1
 
-#./home/ryanicky/workspace/STRTools/scripts/dumpSTR/dumpSTR.py --vcf $vcf \
-#             --out $outdstr \
-#             --filter-regions $filterregions \
-#             --filter-regions-names $filterregionsnames \
-#             --min-total-reads $mintotalreads
+echo $(date '+%Y %b %d %H:%M') dumpSTR ended
 
 
 exit 0 # Not implemented
