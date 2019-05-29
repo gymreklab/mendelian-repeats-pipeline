d87c71d6288f6b76a2485c4ebae74b80acac4257
gymreklab <melissa.gymrek@gmail.com>
2019-05-29 13:56:44 -0700

    updating dockerfile

--- scratch/run_ibd.sh
+++ scratch/run_ibd.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+1;95;0c#!/bin/bash
 
 BAM1=/storage/resources/datasets/WGSdata_LaSpada/LP6008104-DNA_A01/Assembly/LP6008104-DNA_A01.bam
 BAM2=/storage/resources/datasets/WGSdata_LaSpada/LP6008104-DNA_B01/Assembly/LP6008104-DNA_B01.bam
@@ -11,57 +11,60 @@ PRUNESNPS=23andme_snps.bed
 OUTDIR=/storage/mgymrek/del/testIBD/
 
 # Run GATK
-java -jar /storage/resources/source/GenomeAnalysisTK.jar \
-    -R ${REFFASTA} \
-    -T HaplotypeCaller \
-    -I ${BAM1} -I ${BAM2} -I ${BAM3} -I ${BAM4} \
-    -U ALLOW_SEQ_DICT_INCOMPATIBILITY \
-    -L ${PRUNESNPS} \
-    -o ${OUTDIR}/output.raw.snps.indels.vcf
+#java -jar /storage/resources/source/GenomeAnalysisTK.jar \
+#    -R ${REFFASTA} \
+#    -T HaplotypeCaller \
+#    -I ${BAM1} -I ${BAM2} -I ${BAM3} -I ${BAM4} \
+#    -U ALLOW_SEQ_DICT_INCOMPATIBILITY \
+#    -L ${PRUNESNPS} \
+#    -o ${OUTDIR}/output.raw.snps.indels.vcf
 
 # Select SNPs
-java -jar /storage/resources/source/GenomeAnalysisTK.jar \
-    -R ${REFFASTA} \
-    -T SelectVariants \
-    -V ${OUTDIR}/output.raw.snps.indels.vcf \
-    -selectType snp \
-    -o ${OUTDIR}/output.raw.snps.vcf
+#java -jar /storage/resources/source/GenomeAnalysisTK.jar \
+#    -R ${REFFASTA} \
+#    -T SelectVariants \
+#    -V ${OUTDIR}/output.raw.snps.indels.vcf \
+#    -selectType snp \
+#    -o ${OUTDIR}/output.raw.snps.vcf
 
 # Filter SNPs
-java -jar /storage/resources/source/GenomeAnalysisTK.jar \
-    -R ${REFFASTA} \
-    -T VariantFiltration \
-    -V ${OUTDIR}/output.raw.snps.vcf \
-    --filterExpression "DP<20 || QD < 2.0 || FS > 60.0 || MQ < 40.0" \
-    --filterName "snp_filter" \
-    --setFilteredGtToNocall \
-    -o ${OUTDIR}/output.filtered.snps.vcf
+#java -jar /storage/resources/source/GenomeAnalysisTK.jar \
+#    -R ${REFFASTA} \
+#    -T VariantFiltration \
+#    -V ${OUTDIR}/output.raw.snps.vcf \
+#    --filterExpression "DP<20 || QD < 2.0 || FS > 60.0 || MQ < 40.0" \
+#    --filterName "snp_filter" \
+#    --setFilteredGtToNocall \
+#    -o ${OUTDIR}/output.filtered.snps.vcf
 
 # Phase SNPs 
-java -jar /storage/mgymrek/del/testIBD/beagle.r1399.jar \
-    gt=${OUTDIR}/output.filtered.snps.vcf \
-    ped=${OUTDIR}/test.fam \
-    out=${OUTDIR}/output.phased
+#java -jar /storage/mgymrek/del/testIBD/beagle.r1399.jar \
+#    gt=${OUTDIR}/output.filtered.snps.vcf \
+#    ped=${OUTDIR}/test.fam \
+#    out=${OUTDIR}/output.phased
 
 # Convert to plink format
-plink \
-    --vcf ${OUTDIR}/output.phased.vcf.gz --keep-allele-order \
-    --id-delim _ \
-    --recode --snps-only --biallelic-only \
-     --out ${OUTDIR}/output.phased
+#plink \
+#    --vcf ${OUTDIR}/output.phased.vcf.gz --keep-allele-order \
+#    --id-delim _ \
+#    --recode --snps-only --biallelic-only --geno 0 \
+#     --out ${OUTDIR}/output.phased
 
-# Germline
-/storage/mgymrek/del/testIBD/germline \
-    -input ${OUTDIR}/output.phased.ped ${OUTDIR}/output.phased.map \
-    -min_m 0.1 -err_hom 20 -err_het 10 \
-    -bits 64 \
-    -output ${OUTDIR}/output
+# Split by chrom
+for chrom in $(seq 1 23)
+do
+    plink --file ${OUTDIR}/output.phased \
+	--chr ${chrom} \
+	--out ${OUTDIR}/output.phased.chr${chrom} \
+	--recode
 
-#cat output.match
-echo "GF GM" $(cat ${OUTDIR}/output.match  | grep A01 | grep B01 | cut -f 7 | datamash sum 1)
-echo "GF F" $(cat ${OUTDIR}/output.match  | grep A01 | grep C01 | cut -f 7 | datamash sum 1)
-echo "GM F" $(cat ${OUTDIR}/output.match  | grep C01 | grep B01 | cut -f 7 | datamash sum 1)
-echo "F S" $(cat ${OUTDIR}/output.match  | grep C01 | grep D01 | cut -f 7 | datamash sum 1)
-echo "GF S" $(cat ${OUTDIR}/output.match  | grep A01 | grep D01 | cut -f 7 | datamash sum 1)
-echo "GM S" $(cat ${OUTDIR}/output.match  | grep B01 | grep D01 | cut -f 7 | datamash sum 1)
+    # Germline
+    /storage/mgymrek/del/testIBD/germline \
+	-input ${OUTDIR}/output.phased.chr${chrom}.ped ${OUTDIR}/output.phased.chr${chrom}.map \
+	-output ${OUTDIR}/output.chr${chrom}
+#	-min_m 0.1 -err_hom 20 -err_het 10 \
+#	-bits 64 \
+done
 
+# java -jar refined-ibd.16May19.ad5.jar gt=/storage/mgymrek/del/testIBD/output.phased.vcf.gz out=test window=5 length=1
+./summarize_ibd.sh
